3265e8995cc437a284d83a4891e39af55c0cac6d
linaro <luca@sma-rty.com>
2020-12-26 16:39:59 +0000

    added opencv2 support. Dataset generator setup

--- examples/first-frame/main.cpp
+++ examples/first-frame/main.cpp
@@ -34,16 +34,69 @@
 #include <aditof/system.h>
 #include <glog/logging.h>
 #include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+
+#include <dirent.h>
+#include <time.h>
+#include <signal.h>
 
 using namespace aditof;
+
+cv::VideoWriter outvideo;
+
+std::string getTimeString()
+{
+    time_t unixTime = time(NULL);
+    struct tm *locTime = localtime(&unixTime);
+
+    std::string str;
+
+    str = std::to_string(locTime->tm_year - 100);
+
+    if ((locTime->tm_mon+1) < 10) str += "0";
+    str += std::to_string(locTime->tm_mon + 1);
+
+    if (locTime->tm_mday < 10) str += "0";
+    str += std::to_string(locTime->tm_mday);
+
+    str += "_";
+
+    if (locTime->tm_hour < 10) str += "0";
+    str += std::to_string(locTime->tm_hour);
+
+    if (locTime->tm_min < 10) str += "0";
+    str += std::to_string(locTime->tm_min);
+
+    if (locTime->tm_sec < 10) str += "0";
+    str += std::to_string(locTime->tm_sec);
+
+    return str;
+}
+
+static void exit_handler(int s)
+{
+    LOG(ERROR) << "Caught signal " << s;
+
+	//outvideo.release();
+
+    exit(1);
+}
 
 int main(int argc, char *argv[]) {
 
     google::InitGoogleLogging(argv[0]);
     FLAGS_alsologtostderr = 1;
 
-    Status status = Status::OK;
 
+	struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = exit_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    Status status = Status::OK;
+    
     System system;
     status = system.initialize();
     if (status != Status::OK) {
@@ -90,33 +143,87 @@ int main(int argc, char *argv[]) {
     }
 
     aditof::Frame frame;
-
+    
     status = camera->requestFrame(&frame);
-    if (status != Status::OK) {
-        LOG(ERROR) << "Could not request frame!";
+	if (status != Status::OK) {
+		LOG(ERROR) << "Could not request frame!";
+		return 0;
+	} else {
+		//LOG(INFO) << "succesfully requested frame!"; 
+	}
+
+	FrameDetails fDetails;
+	frame.getDetails(fDetails);
+	
+	int frameHeight = static_cast<int>(fDetails.height) / 2;
+	int frameWidth = static_cast<int>(fDetails.width);
+	int currentFrame;
+	int rangeMax = 1200;
+	int rangeMin = 500;
+    
+    std::string timestamp_str = getTimeString();
+    
+    outvideo.open(timestamp_str + "_orig.avi", 
+					CV_FOURCC('X','V','I','D'), 
+					15.0, 
+					cv::Size(frameWidth, frameHeight), 
+					true);
+					
+    if (!outvideo.isOpened())
+    {
+        LOG(ERROR) << "Error opening outvideo video";
         return 0;
-    } else {
-        LOG(INFO) << "succesfully requested frame!";
     }
+    
+    currentFrame = 0;
+    
+    while(1)
+    {
 
-    uint16_t *data1;
-    status = frame.getData(FrameDataType::RAW, &data1);
+		status = camera->requestFrame(&frame);
+		if (status != Status::OK) {
+			LOG(ERROR) << "Could not request frame!";
+			return 0;
+		} else {
+			//LOG(INFO) << "succesfully requested frame!"; 
+		}
 
-    if (status != Status::OK) {
-        LOG(ERROR) << "Could not get frame data!";
-        return 0;
-    }
+		uint16_t *data;
+		status = frame.getData(FrameDataType::RAW, &data);
 
-    if (!data1) {
-        LOG(ERROR) << "no memory allocated in frame";
-        return 0;
-    }
+		if (status != Status::OK) {
+			LOG(ERROR) << "Could not get frame data!";
+			return 0;
+		}
 
-    FrameDetails fDetails;
-    frame.getDetails(fDetails);
-    for (unsigned int i = 0; i < fDetails.width * fDetails.height; ++i) {
-        std::cout << data1[i] << " ";
-    }
+		if (!data) {
+			LOG(ERROR) << "no memory allocated in frame";
+			return 0;
+		}
+	
+		std::ofstream outbin (timestamp_str + "_" + std::to_string(currentFrame) + ".bin");
+		
+		cv::Mat m_depthImage = cv::Mat(cv::Size(frameWidth, frameHeight), CV_16UC1, data);
+			 
+		outbin.write( (char*) m_depthImage.data, 
+					m_depthImage.elemSize() * m_depthImage.total());
+		
+		m_depthImage.convertTo(
+				m_depthImage, CV_8U,
+				(255.0 / (rangeMax - rangeMin)),
+				(-(255.0 / (rangeMax - rangeMin)) * rangeMin));
+		
+		cv::applyColorMap(m_depthImage, m_depthImage, cv::COLORMAP_RAINBOW);
+		cv::flip(m_depthImage, m_depthImage, 1);
+
+		//cv::imshow( "TEST", m_depthImage);
+		outvideo << m_depthImage;
+		
+		currentFrame++;
+		outbin.close();
+		
+		//cv::waitKey(1);
+	}
 
     return 0;
 }
